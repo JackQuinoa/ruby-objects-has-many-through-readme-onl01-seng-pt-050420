002e2d3a95ec0bcb601a478e37d9b9a2cff598ac
Jack Quinoa <jquinoa208@gmail.com>
2020-06-09 19:18:59 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,17 @@
 class Waiter
+  
+  attr_accessor :name, :yrs_experince
+  
+  @@all - []
+  
+  def initialize(nmae, yrs_experince)
+    @name = name
+    @yrs_experince = yrs_experince
+    @@all << self 
+  end
+  
+  def self.all 
+    @@all 
+  end
 
 end
